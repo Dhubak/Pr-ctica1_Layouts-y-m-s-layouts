fbc312f4fbe445048fc67c35154d612615d61121
Dhubak <80739038+Dhubak@users.noreply.github.com>
2021-03-18 00:02:29 -0700

    Add files via upload

---
+++ LADM_U1_PRACTICA1/app/src/main/java/mx/tecnm/tepic/ladm_u1_practica1/Main4Activity.kt
@@ -0,0 +1,12 @@
+package mx.tecnm.tepic.ladm_u1_practica1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Main4Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+    }
+}
